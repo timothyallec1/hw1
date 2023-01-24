d4d6b2d9d8d45bb76698e2c77d6c8d5e1127e1e7
timothyallec1 <timothy.allec@calbaptist.edu>
2023-01-23 22:30:55 -0800

    hw1 first commit

--- hw1-HW1-STARTER-MATERIALS/src/HtmlValidator.java
+++ hw1-HW1-STARTER-MATERIALS/src/HtmlValidator.java
@@ -1,6 +1,82 @@
 /**
  * Add your own comments
  */
+import java.util.*;
+
 public class HtmlValidator {
-    //IMPLEMENT YOUR CODE HERE
+
+    private Queue<HtmlTag> tagsList;
+
+   // creates an empty queue
+    public HtmlValidator() {
+        tagsList = new LinkedList<>();
+    }
+
+     //Create an HtmlValidator with the content of tags
+     //creates a copy of the passed in queue
+
+    public HtmlValidator(Queue<HtmlTag> tags) {
+        this.tagsList = new LinkedList<>(tags);
+        if (tags == null) {
+            throw new IllegalArgumentException("cannot be null.");
+        }
+    }
+
+    // this adds tag to HtmlValidator
+    public void addTag(HtmlTag tag) {
+        if (tag == null) {
+            throw new IllegalArgumentException("Cannot add a null tag");
+        }
+        tagsList.add(tag);
+    }
+
+    // returns copy of the tags in HtmlValidator
+    public Queue<HtmlTag> getTags() {
+        // return a deep copy of tags
+        return new LinkedList<>(tagsList);
+    }
+
+    // this removes any elements that matches each other
+    public void removeAll(String element) {
+        if (element == null) throw new IllegalArgumentException();
+        tagsList.removeIf(tag -> tag.getElement().equalsIgnoreCase(element));
+
+    }
+
+    // prints out properly formatted HTML code from HtmlValidator
+    public void validate() {
+        Stack<HtmlTag> openTags = new Stack<>();
+        for(int i = 0; i < tagsList.size(); i++) {
+            HtmlTag tag = tagsList.remove();
+            tagsList.add(tag);
+
+            if (tag.isSelfClosing()) {
+                printWithIndentation(tag, openTags.size());
+            } else if (tag.isOpenTag()) {
+                printWithIndentation(tag, openTags.size());
+                openTags.push(tag);
+            } else if (!openTags.isEmpty() && tag.matches(openTags.peek())) { // By exhaustion, the tag must be a closing tag
+                // Closing tag should be at same depth as opening, so we pop before printing
+                openTags.pop();
+                printWithIndentation(tag, openTags.size());
+            } else {
+                System.out.println("ERROR unexpected tag: " + tag.toString());
+            }
+        }
+        // Deal with unclosed tags
+        while (!openTags.isEmpty()) {
+            HtmlTag tag = openTags.pop();
+            System.out.println("ERROR unclosed tag: " + tag.toString());
+        }
+    }
+
+    // Helper function to make printing at given indentation more convenient
+    private static void printWithIndentation(HtmlTag tag, int indentationLevel) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < indentationLevel; i++) {
+            sb.append("    ");
+        }
+        sb.append(tag.toString());
+        System.out.println(sb.toString());
+    }
 }
--- hw1-HW1-STARTER-MATERIALS/tst/HtmlValidatorTest.java
+++ hw1-HW1-STARTER-MATERIALS/tst/HtmlValidatorTest.java
@@ -9,10 +9,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class HtmlValidatorTest {
@@ -61,7 +61,7 @@ public class HtmlValidatorTest {
      * of testing your validate() method
      * <b>Hi</b><br/> is the hypothetical html file to test
      */
-    @Test
+   /* @Test
     public void test0(){
         //<b>Hi</b><br/>
         Queue<HtmlTag> tags = new LinkedList<>();
@@ -71,8 +71,48 @@ public class HtmlValidatorTest {
         HtmlValidator validator = new HtmlValidator(tags);
 
         //Note test0_expected_output.txt is placed under tst. Use relative path!
-        Assert.assertEquals(expectedOutputToString("tst/test0_expected_output.txt"),
+        Assert.assertEquals(expectedOutputToString("/Users/dartagnancalitz/Documents/EGR227-SP/EGR227Hw1/EGR227-HW1/tst/test_expected_output0.txt"),
                             validatorOutputToString(validator));
+    }
+    */
+
+    private static final String EXPECTED_TEMPLATE = "hw1-HW1-STARTER-MATERIALS/expected_output/validate_result_for_test%d.txt";
+    private static final String INPUT_TEMPLATE = "hw1-HW1-STARTER-MATERIALS/input_html/test%d.html";
+
+    private static void testAgainstFiles(int testNumber) {
+        testValidatorWithFiles(String.format(EXPECTED_TEMPLATE, testNumber), String.format(INPUT_TEMPLATE, testNumber));
+    }
+
+    private static void testValidatorWithFiles(String expectedOutputFilePath, String validatorInputFilePath) {
+        String rawInput = dumpFileContentsToString(validatorInputFilePath);
+        String expected = dumpFileContentsToString(expectedOutputFilePath);
+        HtmlValidator validator = new HtmlValidator(HtmlTag.tokenize(rawInput));
+
+        String validatorOutput = captureValidatorOutput(validator);
+
+        Assert.assertEquals("Validator output must match expected value", expected, validatorOutput);
+    }
+
+    private static String captureValidatorOutput(HtmlValidator validator) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        PrintStream oldOut = System.out;
+        System.setOut(ps);
+
+        validator.validate();
+
+        System.out.flush();
+        System.setOut(oldOut);
+        return baos.toString();
+    }
+
+    private static String dumpFileContentsToString(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            Assert.fail("Could not load file: " + filePath);
+            return null;
+        }
     }
 
     /**
@@ -80,118 +120,82 @@ public class HtmlValidatorTest {
      * reproducing the test of
      * input_html/test1.html and expected_output/validate_result_for_test1.txt
      */
-	@Test
-	public void test1(){
 
-	}
-
-    /**
-     * This test2 method should test your validate() method
-     * reproducing the test of
-     * input_html/test2.html and expected_output/validate_result_for_test2.txt
-     */
-	@Test
-	public void test2(){
-
-	}
-
-
-    /**
-     * This test3 method should test your validate() method
-     * reproducing the test of
-     * input_html/test3.html and expected_output/validate_result_for_test3.txt
-     */
-	@Test
-	public void test3(){
-
-	}
-
-
-    /**
-     * This test4 method should test your validate() method
-     * reproducing the test of
-     * input_html/test4.html and expected_output/validate_result_for_test4.txt
-     */
-	@Test
-	public void test4(){
-
-	}
-
-    /**
-     * This test5 method should test your validate() method
-     * reproducing the test of
-     * input_html/test5.html and expected_output/validate_result_for_test5.txt
-     */
-	@Test
-	public void test5(){
-
-	}
-
-    /**
-     * This test1 method should test your validate() method
-     * reproducing the test of
-     * input_html/test6.html and expected_output/validate_result_for_test6.txt
-     */
-	@Test
-	public void test6(){
-
-	}
-
-    /**
-     * This test7 method should test your validate() method
-     * reproducing the test of
-     * input_html/test7.html and expected_output/validate_result_for_test7.txt
-     */
-	@Test
-	public void test7(){
-
-	}
-
-    /**
-     * This test8 method should test your validate() method
-     * reproducing the test of
-     * input_html/test8.html and expected_output/validate_result_for_test8.txt
-     */
-	@Test
-	public void test8(){
-
-	}
-
-	/**
-	 * Add your own test to test your removeAll method
-	 * Add your own comment here:
-	 */
-	@Test
-	public void myRemoveAllTest1(){
-
-	}
-
-	/**
-	 * Add your own test to test your removeAll method
-	 * Add your own comment here:
-	 */
-	@Test
-	public void myRemoveAllTest2(){
-
-	}
-
-	/**
-	 * Add your own test to test your removeAll method
-	 * Add your own comment here:
-	 */
-	@Test
-	public void myRemoveAllTest3(){
-
-	}
-
-    /**
-     * Add your own test to test your removeAll method
-     * Add your own comment here:
-     */
     @Test
-    public void myRemoveAllTest4(){
+    public void test1() {
+        testAgainstFiles(1);
 
     }
 
-    //FEEL FREE TO ADD MORE TESTS HERE
+    @Test
+    public void test2() {
+        testAgainstFiles(2);
+    }
+
+    @Test
+    public void test3() {
+        testAgainstFiles(3);
+    }
+
+    @Test
+    public void test4() {
+        testAgainstFiles(4);
+    }
+
+    @Test
+    public void test5() {
+        testAgainstFiles(5);
+    }
+
+    @Test
+    public void test6() {
+        testAgainstFiles(6);
+    }
+
+    @Test
+    public void test7() {
+        testAgainstFiles(7);
+    }
+
+    @Test
+    public void test8() {
+        testAgainstFiles(7);
+    }
+
+    @Test
+    public void addTagTest() {
+        HtmlTag[] tagsArr = {new HtmlTag("Hello"), new HtmlTag("There")};
+        List<HtmlTag> tags = new ArrayList<>(Arrays.asList(tagsArr));
+        HtmlValidator validator = new HtmlValidator();
+
+        tags.forEach(validator::addTag);
+
+        Assert.assertEquals(tags, validator.getTags());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addNullTagTest() {
+        HtmlValidator validator = new HtmlValidator();
+        validator.addTag(null);
+    }
+
+    @Test
+    public void removeAllTest() {
+        HtmlTag[] tagsArr = {new HtmlTag("Hello"), new HtmlTag("There")};
+        List<HtmlTag> tags = new ArrayList<>(Arrays.asList(tagsArr));
+        HtmlValidator validator = new HtmlValidator();
+        tags.forEach(validator::addTag);
+        validator.addTag(new HtmlTag("General Kenobi"));
+
+        validator.removeAll("General Kenobi");
+
+        Assert.assertEquals(tags, validator.getTags());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removeAllNullTest() {
+        HtmlValidator validator = new HtmlValidator();
+
+        validator.removeAll(null);
+    }
 }
